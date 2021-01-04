beab32d3333f1c4955de53c62eec3d4e2e8be8b5
Rashik <58215479+Rashik4567@users.noreply.github.com>
2021-01-04 17:06:48 +0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 
